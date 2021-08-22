9bc774f902326782b26abbdcb112ecb54c19ef88
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-08-22 08:03:30 +0530

    Update Merge Intervals.py

--- Array Problems/Merge Intervals.py
+++ Array Problems/Merge Intervals.py
@@ -1,1 +1,25 @@
 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        
+        
+        intervals=sorted(intervals)
+        output=intervals[0]
+
+        res=[]
+
+        for i in range(1,len(intervals)):
+            if intervals[i][0]<output[1]:
+                output[1]=max(output[1],intervals[i][1])
+
+            else:
+                res.append(output.copy())
+
+                output[0]=intervals[i][0]
+                output[1]=intervals[i][1]
+
+
+        res.append(output)
+        return res
+                   
