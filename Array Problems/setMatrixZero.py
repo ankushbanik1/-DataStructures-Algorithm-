7be9ab8296dae8d9ddc87d1110b1ff2b28115fa2
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-08-22 09:15:54 +0530

    Create setMatrixZero.py

---
+++ Array Problems/setMatrixZero.py
@@ -0,0 +1,19 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+
+        col = []
+        
+        for i in range(len(matrix)):
+            flag = False
+            for j in range(len(matrix[0])):
+                if not matrix[i][j]:
+                    flag = True
+                    if j not in col:
+                        col.append(j)
+            if flag:
+                matrix[i] = [0 for _ in range(len(matrix[0]))]
+
+
+        for each in col:
+            for i in range(len(matrix)):
+                matrix[i][each] = 0
