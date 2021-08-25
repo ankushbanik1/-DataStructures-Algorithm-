1f6550c541f404f990a3cf3bcbc7664d23ee875d
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-08-25 08:15:06 +0530

    Create kth largest - smallest eliment in Array.py

---
+++ Array Problems/kth largest - smallest eliment in Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        return sorted(nums)[k]
+      
+      
+#       smallest
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        return sorted(nums)[0]
