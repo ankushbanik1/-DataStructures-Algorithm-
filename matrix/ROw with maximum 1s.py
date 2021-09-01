2de966c17af19844714e0b318145402c1da25841
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-09-02 00:38:38 +0530

    Create ROw with maximum 1s.py

---
+++ matrix/ROw with maximum 1s.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+
+	def rowWithMax1s(self,arr, n, m):
+		# code here
+	
+		
+		
+		    
+		for i in range(m):
+		    for j in range(n):
+		        
+		    
+    		    if arr[j][i]==1:
+    		         return j
+    		        
+    		        
+		return -1
+	        
+		
