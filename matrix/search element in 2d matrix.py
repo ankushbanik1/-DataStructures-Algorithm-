f35c051b32cbc398a454a6d5dfadde157020f4f8
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-09-02 00:40:04 +0530

    Create search element in 2d matrix.py

---
+++ matrix/search element in 2d matrix.py
@@ -0,0 +1,26 @@
+ def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        n=len(matrix)
+        
+        m=len(matrix[0])
+        
+        if n<=0:
+            return false
+        
+        hi=(n*m)-1
+        low=0
+        
+        while (low<=hi):
+            mid=(low+(hi-low)//2)
+            
+            
+            if matrix[mid//m][mid%m]==target:
+                return True
+            elif matrix[mid//m][mid%m]<target:
+                low=mid+1
+            else:
+                
+                hi=mid-1
+                
+                
+        return False        
