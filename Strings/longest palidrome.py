2b1553cace09eac8fe3c1f8fb4ec6ba855893359
Ankush Banik <43584522+ankushbanik1@users.noreply.github.com>
2021-09-07 00:06:47 +0530

    Create longest palidrome.py

---
+++ Strings/longest palidrome.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    
+    def longestPalindrome(self, s):
+        
+        if(len(s)==1):
+            return(s)
+        if(s==s[::-1]):
+            return(s)
+        else:
+            c=s[0]
+            for i in range(len(s)):
+                x=""
+                for j in s[i:]:
+                    x=x+j
+                    y=x[::-1]
+                    if(x==y):
+                        if(len(x)>len(c)):
+                            c=x
+            return(c)
